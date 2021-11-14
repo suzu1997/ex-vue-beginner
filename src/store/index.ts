92de5fd0e44a38698a78a7b9b801ebcc184f16ca
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-14 16:17:05 +0900

    演習7のVuexストアの作成

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,23 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import { Student } from '@/types/student';
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  strict: true,
+  state: {
+    student: [
+      new Student(10, 'イチロー', 21, 1),
+      new Student(20, 'ジロー', 32, 2),
+      new Student(30, 'サブロー', 43, 1),
+    ],
+  },
+  // getters: {
+  //   getSearchStudentByName(name: string) {
+  //     return 
+  //   }
+  // },
   mutations: {},
   actions: {},
   modules: {},
