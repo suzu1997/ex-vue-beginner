32239fa20328e37e19833d13f4d0880a5294b5a9
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-14 16:34:10 +0900

    検索結果を表示する機能を実装

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,17 +7,21 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
-    student: [
+    students: [
       new Student(10, 'イチロー', 21, 1),
       new Student(20, 'ジロー', 32, 2),
       new Student(30, 'サブロー', 43, 1),
     ],
   },
-  // getters: {
-  //   getSearchStudentByName(name: string) {
-  //     return 
-  //   }
-  // },
+  getters: {
+    getSearchStudentByName(state) {
+      return (inputName: string) => {
+        return state.students.filter((student) =>
+          student.name.includes(inputName)
+        );
+      };
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
--- src/views/Ex07.vue
+++ src/views/Ex07.vue
@@ -2,11 +2,16 @@
   <div>
     <h1>従業員検索</h1>
     <label for="name">名前：</label>
-    <input type="text" id="name" />
+    <input type="text" id="name" v-model="inputName" />
     <br />
-    <button>検索</button>
+    <button v-on:click="search">検索</button>
     <hr />
-    <h2>検索結果({{}}件)</h2>
+    <h2>検索結果({{ searchResult.length }}件)</h2>
+    <ul>
+      <li v-for="student of searchResult" :key="student.id">
+        {{ student.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,7 +19,17 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
-export default class Ex07Component extends Vue {}
+export default class Ex07Component extends Vue {
+  private inputName = '';
+  private searchResult = [];
+
+  search() {
+    this.searchResult = this.getSearchResult;
+  }
+  get getSearchResult() {
+    return this.$store.getters.getSearchStudentByName(this.inputName);
+  }
+}
 </script>
 
 <style></style>
