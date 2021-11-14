89d8eb31aab75015e135a3aa8c9695d4e8cf54d3
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-14 16:16:15 +0900

    演習7のVueファイルを追加し、表示部分を実装

---
+++ src/views/Ex07.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>従業員検索</h1>
+    <label for="name">名前：</label>
+    <input type="text" id="name" />
+    <br />
+    <button>検索</button>
+    <hr />
+    <h2>検索結果({{}}件)</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Ex07Component extends Vue {}
+</script>
+
+<style></style>
